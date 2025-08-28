b5a3ef4738232a7a858e8a6c54e2337d3369ee66
isabelmaito <maitoisabel@gmail.com>
2025-08-27 21:09:29 -0300

    Exercicio à corrigir

---
+++ my-app/App3.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react"
+import { View, Text, TextInput, StyleSheet } from "react-native"
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: 'Delbicos'
+        };
+
+        this.pegaNome = this.pegaNome.bind(this);
+    }
+
+    pegaNome(texto){
+      if(texto.lenght > 0 ){
+        this.setState({nome: 'Bem Vindo' + texto});
+      }else{
+        this.setState({nome: ''});
+      }
+    }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <TextInput 
+        style={styles.input}
+        placeholder="Digite seu nome"
+        underlineColorAndroid="transparent"
+        onChangeText={ (texto) => this.setState({input: texto})}
+        />
+        <Button title="Entrar" onPress={this.entrar}/>
+        
+        <Text style={styles.texto}>{this.state.nome}</Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'space-around'
+    },
+
+    input:{
+        height: 45,
+        borderWidth: 1,
+        borderColor: '#0000',
+        margin: 10,
+        fontSize: 20,
+        padding: 10,
+        backgroundColor: '#aaa'
+    },
+
+    texto:{
+        textAlign: 'center',
+        fontSize: 25
+    }
+})
+
+export default App;
+
