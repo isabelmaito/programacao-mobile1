4804eee5bb338b557cd340ee0ff3c16547c6d395
isabelmaito <maitoisabel@gmail.com>
2025-10-08 20:50:24 -0300

    fix: corrigir caminho de importação do componente Cesta e adicionar mock de dados

--- my-app-cesta/App.js
+++ my-app-cesta/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Cesta from './src/telas/telas/cesta';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar, View } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Cesta from './src/telas/cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-        <Cesta/>
+      <StatusBar />
+      <Cesta {...mock} />
     </SafeAreaProvider>
   );
 }
