4a9b08eb07132f371ba87e2a28a160a2749465f9
isabelmaito <maitoisabel@gmail.com>
2025-11-19 19:56:15 -0300

    feat: reestruturar o aplicativo para usar navegação com pilha e adicionar telas de Cadastro e Consulta

--- my-app-5/App.js
+++ my-app-5/App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; //
+import { NavigationContainer } from '@react-navigation/native'; //
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; //
 
-export default function App() {
+// Importando as telas
+import CadastroScreen from './screens/CadastroScreen'; //
+import ConsultaScreen from './screens/ConsultaScreen'; //
+import HomeScreen from './screens/HomeScreen'; //
+
+const Stack = createNativeStackNavigator(); //
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer> //
+      <Stack.Navigator initialRouteName="Home"> //
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Página Inicial' }} 
+        /> //
+        <Stack.Screen 
+          name="Cadastro" 
+          component={CadastroScreen} 
+        /> //
+        <Stack.Screen 
+          name="Consulta" 
+          component={ConsultaScreen} 
+        /> //
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); //
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App; //
