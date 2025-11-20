551103cd27210a5574d2ab3f0b111ab376f1b737
isabelmaito <maitoisabel@gmail.com>
2025-11-19 22:20:22 -0300

    refactor: corrigir rotas e melhorar a estrutura do servidor

--- my-app-5/server.js
+++ my-app-5/server.js
@@ -1,17 +1,30 @@
-const express = require('express'); //
-const bodyParser = require('body-parser'); //
-const userRoutes = require('./rotas/userRoutes'); //
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const tarefasRoutes = require('./rotas/tarefasRoutes'); 
 
 const app = express();
-const PORT = 3000; //
+const PORT = 3000; 
 
 // Middleware
-app.use(bodyParser.json()); //
+app.use(bodyParser.json()); 
+
+// CORS livre
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 // Rotas
-app.use('/api', userRoutes); //
+app.get('/ping', (req, res) => res.send('API de Tarefas rodando'));
+app.use('/api', tarefasRoutes); 
 
 // Iniciar o servidor
-app.listen(PORT, () => { //
-    console.log(`Servidor rodando na porta ${PORT}`); //
+app.listen(PORT, () => { 
+    console.log(`Servidor rodando na porta ${PORT}`); 
 });
