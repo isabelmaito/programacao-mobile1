8f0ee68123cebd1b1a99896066ea939ed320a528
isabelmaito <maitoisabel@gmail.com>
2025-11-19 21:16:51 -0300

    feat: reorganizar importações e adicionar telas de alteração e apagar

--- my-app-5/App.js
+++ my-app-5/App.js
@@ -1,34 +1,41 @@
 import React from 'react'; //
 import { NavigationContainer } from '@react-navigation/native'; //
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; //
+import AtualizaScreen from './screens/AtualizaScreen';
+import DeleteScreen from './screens/DeleteScreen';
+import CadastroScreen from './screens/CadastroScreen';
+import ConsultaScreen from './screens/ConsultaScreen';
+import HomeScreen from './screens/HomeScreen'; //Import da nova tela
 
-// Importando as telas
-import CadastroScreen from './screens/CadastroScreen'; //
-import ConsultaScreen from './screens/ConsultaScreen'; //
-import HomeScreen from './screens/HomeScreen'; //
-
-const Stack = createNativeStackNavigator(); //
-
+const Stack = createNativeStackNavigator(); 
 const App = () => {
   return (
-    <NavigationContainer> //
-      <Stack.Navigator initialRouteName="Home"> //
+    <NavigationContainer> 
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
           options={{ title: 'Página Inicial' }} 
-        /> //
+        /> 
         <Stack.Screen 
           name="Cadastro" 
           component={CadastroScreen} 
-        /> //
+        /> 
         <Stack.Screen 
           name="Consulta" 
           component={ConsultaScreen} 
-        /> //
+        /> 
+        <Stack.Screen
+          name="Alteração"
+          component={AtualizaScreen}
+        />
+        <Stack.Screen
+          name="Apagar"
+          component={DeleteScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  ); //
+  ); 
 };
 
-export default App; //
+export default App; 
