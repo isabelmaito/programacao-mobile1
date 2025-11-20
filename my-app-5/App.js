a6ef62935f903a54fa80dad58992388052867068
isabelmaito <maitoisabel@gmail.com>
2025-11-19 22:21:12 -0300

    feat: simplificar a navegação removendo telas não utilizadas e organizando as existentes

--- my-app-5/App.js
+++ my-app-5/App.js
@@ -1,42 +1,21 @@
-import React from 'react'; //
-import { NavigationContainer } from '@react-navigation/native'; //
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; //
-import AtualizacaoScreen from './screens/AtualizacaoScreen';
-import DeleteScreen from './screens/DeleteScreen';
-import CadastroScreen from './screens/CadastroScreen';
-import ConsultaScreen from './screens/ConsultaScreen';
-import HomeScreen from './screens/HomeScreen'; //Import da nova tela
+import React from 'react'; 
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+
+import HomeScreen from './screens/HomeScreen';
+import NovaTarefaScreen from './screens/NovaTarefaScreen';
+import EditarTarefaScreen from './screens/EditarTarefaScreen';
 
 const Stack = createNativeStackNavigator(); 
-console.log("TESTE")
-const App = () => {
-  return (
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ title: 'Página Inicial' }} 
-        /> 
-        <Stack.Screen 
-          name="Cadastro" 
-          component={CadastroScreen} 
-        /> 
-        <Stack.Screen 
-          name="Consulta" 
-          component={ConsultaScreen} 
-        /> 
-        <Stack.Screen
-          name="Alteração"
-          component={AtualizacaoScreen}
-        />
-        <Stack.Screen
-          name="Apagar"
-          component={DeleteScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  ); 
-};
 
-export default App; 
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Tarefas' }} />
+      <Stack.Screen name="NovaTarefa" component={NovaTarefaScreen} options={{ title: 'Nova Tarefa' }} />
+      <Stack.Screen name="EditarTarefa" component={EditarTarefaScreen} options={{ title: 'Editar Tarefa' }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
